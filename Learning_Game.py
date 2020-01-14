f49c7b7099f53a030c068151428c10ea9b48e2df
PreussenBlue <bibbunk@gmail.com>
2020-01-14 09:06:05 +0100

    Update Learning_Game.py

--- Learning_Game.py
+++ Learning_Game.py
@@ -1,5 +1,21 @@
-print("Hello World")
+import pygame
 
-print("Alex is gay")
+pygame.init()
 
-print("No U")
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    pygame.display.flip()
+
+
+pygame.quit()
